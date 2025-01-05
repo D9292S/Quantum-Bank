1bf8993712d2c95d21a877aaea002a18f0d36c14
D9292S <kdeepankar40@gmail.com>
2025-01-05 13:34:36 +0530

    chapter updated

---
+++ test_bot.py
@@ -0,0 +1,16 @@
+import unittest
+from unittest.mock import AsyncMock, patch
+from bot import bot  # Import your bot class
+
+class TestMyBot(unittest.TestCase):
+    def setUp(self):
+        self.bot = bot()
+
+    @patch('discord.ext.commands.Bot.send_message')
+    async def test_ping_command(self, mock_send):
+        ctx = AsyncMock()
+        await self.bot.ping(ctx)
+        mock_send.assert_called_with(ctx, "Pong!")
+
+if __name__ == '__main__':
+    unittest.main()
